c40a8bc8e779c8811223f10ec63d6f9479aa9b24
Nnmdi-p <nnamdiemenike@my.unt.edu>
2026-09-20 17:41:47 -0500

    Update WaitingList.h

--- include/WaitingList.h
+++ include/WaitingList.h
@@ -3,10 +3,12 @@
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 
-// Stores information about one student waiting for a resource
+// Stores the information for one student
+// waiting for a resource.
 struct WaitingRequest {
     int studentID;
     string studentName;
@@ -15,43 +17,76 @@ struct WaitingRequest {
 };
 
 
-// Node used to build the waiting list queue
+// Node used to build the waiting list queue.
 struct WaitingNode {
     WaitingRequest data;
     WaitingNode* next;
 
     WaitingNode(const WaitingRequest& request)
-        : data(request), next(nullptr) {}
+        : data(request), next(nullptr) {
+    }
 };
 
 
+// Waiting List Queue
 class WaitingList {
 private:
+
+    // First student in the queue
     WaitingNode* front;
+
+    // Last student in the queue
     WaitingNode* rear;
+
+    // Number of students currently waiting
     int size;
 
+
 public:
+
     // Constructor
     WaitingList();
+
 
     // Destructor
     ~WaitingList();
 
-    // Queue operations
-    void Enqueue(int studentID,
-                 const string& studentName,
-                 const string& resourceID,
-                 const string& reservationDate);
 
-    bool Dequeue(WaitingRequest& removed);
+    // Add a student to the back of the queue
+    void Enqueue(
+        int studentID,
+        const string& studentName,
+        const string& resourceID,
+        const string& reservationDate
+    );
 
+
+    // Remove the student at the front of the queue
+    bool Dequeue(
+        WaitingRequest& removed
+    );
+
+
+    // Find and remove the FIRST student waiting
+    // for a specific resource and date.
+    bool RemoveNextFor(
+        const string& resourceID,
+        const string& reservationDate,
+        WaitingRequest& removed
+    );
+
+
+    // Check whether the queue is empty
     bool IsEmpty() const;
 
+
+    // Return number of students waiting
     int GetSize() const;
+
 
     // Display everyone currently waiting
     void Display() const;
 };
+
 
 #endif
