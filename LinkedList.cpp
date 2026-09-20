8ad5fc9c0b3010a6fbcbbc696e0dba3d9cdfbdff
TLania <taylorllania@gmail.com>
2026-09-20 20:00:40 +0000

    Created LinkedList header and source file.  - Class with necessary functions  - functions defined in source file

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,6 +1,25 @@
+#include "LinkedList.h"
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main() {
-  cout << "Testing!" << endl;
+Reservations::Reservations(){ // Constructor
+   head = nullptr; // Point head to tail node.
+}
+
+Reservations::~Reservations() {
+  Node* current = head;
+  while (current != nullptr){
+    Node* temp = current;
+    current = current->next;
+    delete temp;
+  }
+}
+
+
+// Temporary, TBD.
+int main(){
+
+  return 0;
 }
--- LinkedList.h
+++ LinkedList.h
@@ -3,3 +3,42 @@
 #include <iostream>
 using namespace std;
 
+class Reservations {
+    protected:
+        // Node and all information that is stored in each one.
+        struct Node {
+            int resID; // So reservations do not overlap.
+            string resName;
+            string resDate;
+            int resSize;
+            Node* next; // Pointer that points to next node.
+
+            // Header node to assign values.
+
+            Node(int id, const string& name, const string& date, int size){
+                resID = id;
+                resName = name;
+                resDate = date;
+                resSize = size;
+                next = nullptr; // Points to tail/next node.
+            }
+        };
+
+        // Declare head node
+        Node* head;
+        
+    public:
+        Reservations(); // Default Constructor
+        ~Reservations(); // Destructor
+
+        // Insert, remove, traverse, and display functions.
+        void AddReservation(int id, const string& name, const string& date, int size);
+        bool removeReservation(int reservationID); // Remove reservation of given ID
+        void traverse (void (*visit)(int, const string&, const string&, int)) const;
+        void display() const;
+        
+};
+
+
+#endif
+
